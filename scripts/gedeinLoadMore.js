969e9813797b7b362c8320f268029f5706bfc9f5
risyandi <risyandi.94@gmail.com>
2019-08-09 17:05:18 +0700

    rename and adding

---
+++ scripts/gedeinLoadMore.js
@@ -0,0 +1,21 @@
+class gedeinLoadMore {
+    constructor() {
+        this.elementGedeinLoadMore = null;
+    }
+
+    elementGedeinLoadMore() {
+        let element = `<div class="comment-bubble">
+        <div class="speech-bubble-left aqua">
+            <div class="name-user-comment">Hasan</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            voluptatem quia
+            odio quasi eligendi a, rerum reprehenderit quibusdam quis quam, repellat officiis delectus esse
+            fugit culpa neque rem sint cumque.
+            <img src="https://d28j15pnfa5oxx.cloudfront.net/front_img/default_profile.png"
+                alt="profile-comment">
+        </div>
+    </div>`
+    }
+}
+
+export default loadMore;
