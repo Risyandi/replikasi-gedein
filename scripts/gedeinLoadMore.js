41c394ed2788d7fd70777ce0360ec167de54062d
risyandi <risyandi.94@gmail.com>
2019-08-13 07:16:27 +0700

    adding gedeinJS

--- scripts/gedeinLoadMore.js
+++ scripts/gedeinLoadMore.js
@@ -1,21 +1,34 @@
-class gedeinLoadMore {
-    constructor() {
-        this.elementGedeinLoadMore = null;
-    }
-
-    elementGedeinLoadMore() {
-        let element = `<div class="comment-bubble">
-        <div class="speech-bubble-left aqua">
-            <div class="name-user-comment">Hasan</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-            voluptatem quia
-            odio quasi eligendi a, rerum reprehenderit quibusdam quis quam, repellat officiis delectus esse
-            fugit culpa neque rem sint cumque.
-            <img src="https://d28j15pnfa5oxx.cloudfront.net/front_img/default_profile.png"
-                alt="profile-comment">
-        </div>
-    </div>`
-    }
+window.onload = function () {
+    favouriteComment();
 }
 
-export default loadMore;
+function favouriteComment() {
+    console.log("print element");
+    let element = "",
+        sidebarRightBottom = document.getElementById('sidebar-right-bottom');
+    for (let index = 0; index < 5; index++) {
+        element += `<div class="comment-bubble animate-show">
+                    <div class="speech-bubble-right aqua">
+                        <div class="name-user-comment ${index}">Zulkifli</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+                        voluptatem quia
+                        odio quasi eligendi a, rerum reprehenderit quibusdam quis quam, repellat officiis delectus esse
+                        fugit culpa neque rem sint cumque.
+                        <img src="https://d28j15pnfa5oxx.cloudfront.net/front_img/default_profile.png"
+                            alt="profile-comment">
+                    </div>
+                </div>
+                <div class="comment-bubble animate-show">
+                    <div class="speech-bubble-left aqua">
+                        <div class="name-user-comment ${index}">Hasan</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+                        voluptatem quia
+                        odio quasi eligendi a, rerum reprehenderit quibusdam quis quam, repellat officiis delectus esse
+                        fugit culpa neque rem sint cumque.
+                        <img src="https://d28j15pnfa5oxx.cloudfront.net/front_img/default_profile.png"
+                            alt="profile-comment">
+                    </div>
+                </div>`;
+    }
+    sidebarRightBottom.innerHTML = element;
+}
