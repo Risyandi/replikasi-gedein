0916ffc0844a31ddbca2ed502375633920bcc189
risyandi <risyandi.94@gmail.com>
2019-08-20 14:34:07 +0700

    update gedein

--- scripts/gedeinLoadMore.js
+++ scripts/gedeinLoadMore.js
@@ -1,10 +1,17 @@
 let target = document.getElementById('element');
 let input = document.getElementById('comment');
 let button = document.getElementById('button-comment');
+let listElement = document.querySelector('#sidebar-right-bottom');
 
 window.onload = function () {
-    favouriteComment();
+    loadMore();
 }
+
+listElement.addEventListener("scroll", function () {
+    if (listElement.scrollTop + listElement.clientHeight >= listElement.scrollHeight) {
+        loadMore();
+    }
+});
 
 button.addEventListener("click", function () {
     console.log(button.textContent);
@@ -14,13 +21,14 @@ input.addEventListener("keyup", function () {
     console.log(input.value, 'value input');
 });
 
-function favouriteComment() {
+function loadMore() {
     let element = "",
-        sidebarRightBottom = document.getElementById('sidebar-right-bottom');
-    for (let index = 0; index < 5; index++) {
+        start = 0,
+        limit = 3;
+    for (let index = start; index < limit; index++) {
         element += `<div class="comment-bubble animate-show">
                     <div class="speech-bubble-right aqua">
-                        <div class="name-user-comment ${index}">Zulkifli</div>
+                        <div class="name-user-comment">Down Johnson</div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
                         voluptatem quia
                         odio quasi eligendi a, rerum reprehenderit quibusdam quis quam, repellat officiis delectus esse
@@ -30,16 +38,18 @@ function favouriteComment() {
                     </div>
                 </div>
                 <div class="comment-bubble animate-show">
-                    <div class="speech-bubble-left aqua">
-                        <div class="name-user-comment ${index}">Hasan</div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-                        voluptatem quia
-                        odio quasi eligendi a, rerum reprehenderit quibusdam quis quam, repellat officiis delectus esse
-                        fugit culpa neque rem sint cumque.
-                        <img src="https://d28j15pnfa5oxx.cloudfront.net/front_img/default_profile.png"
-                            alt="profile-comment">
-                    </div>
+                <div class="speech-bubble-left aqua">
+                    <div class="name-user-comment">Hasan</div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+                    voluptatem quia
+                    odio quasi eligendi a, rerum reprehenderit quibusdam quis quam, repellat officiis delectus esse
+                    fugit culpa neque rem sint cumque.
+                    <img src="https://d28j15pnfa5oxx.cloudfront.net/front_img/default_profile.png"
+                        alt="profile-comment">
+                </div>
                 </div>`;
     }
-    sidebarRightBottom.innerHTML = element;
+    setTimeout(() => {
+        listElement.innerHTML += element;
+    }, 500);
 }
